3b2e7a8977a57f4eab355e04ae6c3a42edf5343e
infoguy <patjohnashkin@gmail.com>
2022-12-23 14:32:44 -0600

    Canvas Assignment

---
+++ story_content/6iSWYXozHqb_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.71.29339.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.273%0D%0AYou%20now%20have%20a%20bunch%20of%20options%20to%20choose%20from%0D%0A%0D%0A"}')
